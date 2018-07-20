f46921361d69e2ffabeb97a7dc6df194baf2dd91
wanggao <wanggao@wanggaodeMacBook-Pro-2.local>
2018-07-20 18:13:43 +0800

    fix

--- pages/vote/lookCommit/lookCommit.js
+++ pages/vote/lookCommit/lookCommit.js
@@ -9,14 +9,14 @@ Page({
       participant_num: 20,
       desc: "This is a test vote",
     },
-    selected_option_ids: ["1", "2"],
+    selected_option_ids: [1, 2],
     items: [
-      { option_id: 1, option_desc: '美国', vote_rate: 20, vote_num: 10 },
-      { option_id: 2, option_desc: '中国', vote_rate: 60, vote_num: 10 },
-      { option_id: 3, option_desc: '巴西', vote_rate: 30, vote_num: 10 },
-      { option_id: 4, option_desc: '日本', vote_rate: 80, vote_num: 10 },
-      { option_id: 5, option_desc: '英国', vote_rate: 1, vote_num: 10 },
-      { option_id: 6, option_desc: '法国', vote_rate: 50, vote_num: 10 },
+      { option_id: 1, option_desc: '美国', vote_rate: 20, vote_num: 10, checked: false },
+      { option_id: 2, option_desc: '中国', vote_rate: 60, vote_num: 10, checked: false },
+      { option_id: 3, option_desc: '巴西', vote_rate: 30, vote_num: 10, checked: false },
+      { option_id: 4, option_desc: '日本', vote_rate: 80, vote_num: 10, checked: false },
+      { option_id: 5, option_desc: '英国', vote_rate: 1, vote_num: 10, checked: false },
+      { option_id: 6, option_desc: '法国', vote_rate: 50, vote_num: 10, checked: false },
     ]
   },
   optionChange: function (e) {
@@ -38,7 +38,16 @@ Page({
   },
 
   onLoad: function (options) {
-    console.log(this.data.selected_option_ids)
-    console.log(this.data.selected_option_ids.indexOf(1))
+    console.log('selected_option_ids', this.data.selected_option_ids)
+    var length = this.data.selected_option_ids.length
+    var that = this
+
+    for (var i = 0; i < length; i++) {
+      console.log('find', this.data.items[i].checked)
+      that.data.items[i].checked = true
+    }
+    this.setData({
+      items: that.data.items
+    })
   }
 })
